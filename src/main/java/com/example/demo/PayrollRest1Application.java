1c2268e567b1429a5ee52f5ef110a291ceee5806
kimisuke <kimisuke0503@gmail.com>
2022-10-10 23:08:11 +0900

    init: spring-rest

---
+++ src/main/java/com/example/demo/PayrollRest1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollRest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollRest1Application.class, args);
+	}
+
+}
